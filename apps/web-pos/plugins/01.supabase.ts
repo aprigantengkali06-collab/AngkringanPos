1e0bac311578f903335011b46fa6e832aa90c01d
aprigantengkali06-collab <aprigantengkali06@gmail.com>
2026-04-05 00:21:03 +0700

    Update 01.supabase.ts

--- apps/web-pos/plugins/01.supabase.ts
+++ apps/web-pos/plugins/01.supabase.ts
@@ -1,1 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+
+export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig()
+  const supabase = createClient(config.public.supabaseUrl, config.public.supabaseAnonKey, {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  })
+
+  return {
+    provide: {
+      supabase
+    }
+  }
+})
 
